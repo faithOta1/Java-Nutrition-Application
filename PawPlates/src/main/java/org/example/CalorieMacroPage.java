0f95e404fe398c44495a01e01003f4a26790dc0f
Dakota Hernandez <128409247+Dakotahernandez@users.noreply.github.com>
2025-03-22 18:38:43 -0500

    Update CalorieMacroPage.java added notes

--- PawPlates/src/main/java/org/example/CalorieMacroPage.java
+++ PawPlates/src/main/java/org/example/CalorieMacroPage.java
@@ -1,1 +1,10 @@
-
+/**
+ * NOTES
+ * WHats done and explanation for why i did certain things
+ * Have set value for calories since we dont have a user logged in with a value
+ * Has 2 menus that pop up has options like logging out or help tabs--
+ * ----None of the buttons do anything
+ * I added a bar across the tops that shows the percent of total
+ * ----calories consumed so far updates evrytime there is a new entry
+ *
+ */
